9159f0c0a906eb5ac9e5221178d607959fae4db9
Your Name <mathis.folschveiller@gmail.com>
2023-12-13 15:24:35 +0100

    peuimporte

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Test extends Component {
+    render() {
+        return (
+            <div className="Test">
+                <h1>Saucisson</h1>
+            </div>
+        );
+    }
+}
+
+export default Test;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react";
+import TestComponent from "./Test";
+import "./index.css";
+
+ReactDOM.render(<TestComponent />, document.getElementById("root"));
